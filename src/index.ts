26f76c8979d0f74b392221a407e2101b6af6d363
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-11 12:34:35 +0100

    user-interaction loop added

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as csv from '@fast-csv/parse';
 import * as readline from 'readline-sync'
 import { format, parse } from 'date-fns';
 
+// MAIN FUNCTIONS
 
 const getData = (fileName: string): void => {
      fs.createReadStream(`./src/lib/CSV/${fileName}`)
@@ -11,14 +12,16 @@ const getData = (fileName: string): void => {
         .on('data', (row: CSVRowData) => {
             const data = parseCSVRowData(row);
             const newTransaction = new Transaction(data)
-            transactions.push(newTransaction)
             getOrCreateAccount(data.from).addTransaction(newTransaction);
             getOrCreateAccount(data.to).addTransaction(newTransaction);
         })
         .on('end', () => {
+            // Update all balances
             accounts.forEach(account => {
                 account.calculateBalance()
             })
+            // Start System
+            console.log(promptUser());
         });
 }
 
@@ -26,9 +29,44 @@ const listAccounts = (): void => {
     accounts.forEach(account => console.log(account.toString()));
 }
 
-const listAccountTransactions = (accountName: string): void => {
-    getOrCreateAccount(accountName).printTransactions();
+const promptUser = (): string => {
+    // Ask for User Input
+    console.log("What would you like to do?")
+    console.log("> List All - Shows all accounts and current balances")
+    console.log("> List [Account Name] - Shows all transactions linked to that account")
+    console.log();
+    const query = readline.question("Please enter your query: ").toLowerCase();
+    console.log();
+    const queryArray = query.split(" ");
+
+    /* 
+        If query is "List All" list all accounts
+        If query is "List [Account Name]" AND account exists, list all transactions
+        If query is "List [Account Name]" AND account does not exist, display no account found error
+        If query is "Exit", exit system
+        If none of the above, display unrecognised prompt error
+    */ 
+    if (query === "list all" || query === "l a") {
+        listAccounts();
+    } else if (queryArray.length === 3 && (queryArray[0] === "list" || queryArray[0] === "l")) {
+        const accountName: string = "" + queryArray.at(1)?.charAt(0).toUpperCase() + queryArray.at(1)?.slice(1) + " " + queryArray[2]?.toUpperCase();
+        if (doesAccountExist(accountName)) {
+            getOrCreateAccount(accountName).printTransactions();
+        } else {
+            console.log(`No Account found for ${accountName}`);
+        }
+    } else if (query === "exit") {
+        return "System Shut Down"
+    } else {
+        console.log(`${query} is not a recognised query`);
+    }
+
+    readline.question("")
+    console.log()
+    return promptUser();
 }
+
+// TYPES and CLASSES
 
 type CSVRowData = {
     date: string;
@@ -62,7 +100,7 @@ class Transaction {
     }
 
     toString() {
-        return `£${this.amount} for ${this.narrative} on the ${format(this.date, "do MM yyyy")}`;
+        return `£${this.amount} for ${this.narrative} on the ${format(this.date, "do MMM yyyy")}`;
     }
 }
 
@@ -95,7 +133,7 @@ class Account {
             if (transaction.to === this.name) {
                 outputString = `(+) Was Paid by ${transaction.from} `
             } else if (transaction.from === this.name) {
-                outputString = `(-) Paid ${transaction.from} `
+                outputString = `(-) Paid ${transaction.to} `
             }
             outputString += transaction.toString();
             console.log(outputString);
@@ -110,11 +148,18 @@ class Account {
                 this.deductBalance(transaction.amount);
             }
         })
+        this.balance = Math.round(this.balance*100)/100
     }
 
     toString() {
-        return this.name + (this.balance >= 0 ? " is owed " : " owes ") + "£" + this.balance
+        return this.name + (this.balance >= 0 ? " is owed " : " owes ") + "£" + Math.abs(this.balance);
     }
+}
+
+// UTIL FUNCTIONS
+
+const doesAccountExist = (accountName: string): boolean => {
+    return accounts.find(account => { return account.name === accountName }) !== undefined;
 }
 
 /**
@@ -148,7 +193,8 @@ const parseCSVRowData = ({ date, from, to, narrative, amount}: CSVRowData): RowD
     };
 }
 
-const transactions: Transaction[] = [];
-const accounts: Account[] = [];
 
+// Run Script
+
+const accounts: Account[] = [];
 getData('Transactions2014.csv');
