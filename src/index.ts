a8db4d8bd94c28b6f6899db04c5b86b16e157a4b
RedHellierSoftwire <red.hellier@softwire.com>
2025-09-11 10:18:58 +0100

    Moved csv reader to function, added calculateBalanace function to Account Class

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,25 @@ import * as fs from 'fs';
 import * as csv from '@fast-csv/parse'
 import { format } from 'date-fns';
 
-const ListAll = (filePath: string) => {
+
+const getData = (fileName: string): void => {
+    fs.createReadStream(`./src/lib/CSV/${fileName}`)
+        .pipe(csv.parse({ headers: true }))
+        .on('error', error => console.error(error))
+        .on('data', (row: RowData) => {
+            getOrAddAccount(row.From);
+            getOrAddAccount(row.To);
+            const newTransaction = new Transaction(row)
+            transactions.push(newTransaction)
+        })
+        .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
+}
+
+const listAccounts = (): void => {
+
+}
+
+const listAccountTransactions = (accountName: string): void => {
 
 }
 
@@ -14,13 +32,32 @@ type RowData = {
     Amount: string;
 }
 
+class Transaction {
+    date: string;
+    from: string;
+    to: string;
+    narrative: string;
+    amount: number;
+
+    constructor(data: RowData) {
+        this.date = format(data.Date, "MM/dd/yyyy");
+        this.from = data.From;
+        this.to = data.To;
+        this.narrative = data.Narrative;
+        this.amount = Number(data.Amount);
+
+    }
+}
+
 class Account {
     name: string;
     balance: number;
+    transactions: Array<Transaction>;
 
     constructor(name: string) {
         this.name = name;
         this.balance = 0;
+        this.transactions = [];
     }
 
     addBalance(transactionAmount: number) : void {
@@ -30,21 +67,15 @@ class Account {
     deductBalance(transactionAmount: number): void {
         this.balance -= transactionAmount;
     }
-}
-class Transaction {
-    date: string;
-    from: string;
-    to: string;
-    narrative: string;
-    amount: number;
 
-    constructor(data: RowData) {
-        this.date = format(data.Date, "yyyy/MM/dd");
-        this.from = data.From;
-        this.to = data.To;
-        this.narrative = data.Narrative;
-        this.amount = Number(data.Amount);
-
+    calculateBalance(): void {
+        this.transactions.forEach(transaction => {
+            if (transaction.to === this.name) {
+                this.addBalance(transaction.amount);
+            } else if (transaction.from === this.name) {
+                this.deductBalance(transaction.amount);
+            }
+        })
     }
 }
 
@@ -52,17 +83,13 @@ class Transaction {
 const transactions: Array<Transaction> = [];
 const accounts: Array<Account> = [new Account("Dan W")];
 
-fs.createReadStream('./src/lib/Transactions2014.csv')
-    .pipe(csv.parse({ headers: true }))
-    .on('error', error => console.error(error))
-    .on('data', (row: RowData) => {
-        getOrAddAccount(row.From);
-        getOrAddAccount(row.To);
-        const newTransaction = new Transaction(row)
-        transactions.push(newTransaction)
-    })
-    .on('end', (rowCount: number) => console.log(`Parsed ${rowCount} rows`));
 
+
+/**
+ * Gets the account by name, or if one does not exist, creates an account and returns that
+ * @param accountName
+ * @returns Either the account if found OR a newly created account
+ */
 const getOrAddAccount = (accountName: string): Account => {
     const account = accounts.find(account => { return account.name === accountName });
     if (!account) {
